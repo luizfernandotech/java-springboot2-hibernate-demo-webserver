a9533d78dc60abeca17d05c26a9e0d89a4310634
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2020-12-05 15:42:59 +0000

    OrderStatus enum

--- src/main/java/com/luizfernandotech/webservice/entities/Order.java
+++ src/main/java/com/luizfernandotech/webservice/entities/Order.java
@@ -1,5 +1,8 @@
 package com.luizfernandotech.webservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.luizfernandotech.webservice.entities.enums.OrderStatus;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
@@ -13,7 +16,11 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -22,9 +29,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -50,6 +58,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
