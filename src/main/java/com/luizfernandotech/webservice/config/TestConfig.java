5955bae82d250afef5babcc1c3022986a43ee344
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2020-12-05 15:52:53 +0000

    Category classes  - Entity  - Repository  - Service  - Resource

--- src/main/java/com/luizfernandotech/webservice/config/TestConfig.java
+++ src/main/java/com/luizfernandotech/webservice/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.luizfernandotech.webservice.config;
 
+import com.luizfernandotech.webservice.entities.Category;
 import com.luizfernandotech.webservice.entities.Order;
 import com.luizfernandotech.webservice.entities.User;
 import com.luizfernandotech.webservice.entities.enums.OrderStatus;
+import com.luizfernandotech.webservice.repositories.CategoryRepository;
 import com.luizfernandotech.webservice.repositories.OrderRepository;
 import com.luizfernandotech.webservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,18 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
